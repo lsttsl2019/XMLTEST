3323f66c8eae2136f6c2a788b657d20721e88ccc
82106 <dlamtd123@naver.com>
2019-05-02 15:41:24 +0900

    XML파싱 작업 완료

--- app/src/main/java/com/isttis2019/ex_xml_test_2018_12_20/MainActivity.java
+++ app/src/main/java/com/isttis2019/ex_xml_test_2018_12_20/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.util.Log;
@@ -217,161 +218,166 @@ public class MainActivity extends AppCompatActivity {
     public void clickBtn(View view) {
 
         if (menber1 != null) menbers.clear();
-
-        Toast.makeText(this, "검색시작", Toast.LENGTH_SHORT).show();
-
-
-        new Thread() {
-            @Override
-            public void run() {
-
-                final String targetDt = se_et.getText().toString();
-                //서버 주소 작성
-                String address = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.xml"
-                        + "?key=" + apikey  //키값
-                        + "&targetDt=" + targetDt//조회하는 날짜
-                        + "&weekGb=0"    //주중으로 받아온다
-                        + "&itemPerPage=10"  //총10개의 데이터를 받아온다.
-                        + "&showRange"     //상영 기간을출력
-                        + "&yearWeekTime"  //조회하는 년도 주차를
-                        + "&rank"           //순위를 츨력을
-                        + "&movieCd"        //영화 대표코드
-                        + "&movieNm"        //영화 이름/
-                        + "&rankOldAndNew"  //영화가 새로들어왔는 기존의 있는지 확인
-                        + "&openDt"         //영화 개봉일 출력
-                        + "&audiCnt"        //해당 일 관객수
-                        + "&audiAcc";         //해당 일 누적관객수 출력
+        if (se_et.length()!=8){
+            dialog.cancel();
+            Toast.makeText(this, "검색년도를 올바르게 입력 해주세요", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "검색시작", Toast.LENGTH_SHORT).show();
 
 
-                try {
-                    URL url = new URL(address);
+            new Thread() {
+                @Override
+                public void run() {
 
-                    InputStream is = url.openStream();
-                    InputStreamReader isr = new InputStreamReader(is);
-
-
-                    //xml분석
-                    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-                    XmlPullParser xpp = factory.newPullParser();
-                    xpp.setInput(isr);
-
-
-                    int eventType = xpp.getEventType();
-
-                    String TagName = null;
-
-
-                    while (eventType != XmlPullParser.END_DOCUMENT) {
-
-                        switch (eventType) {
-                            case XmlPullParser.START_DOCUMENT:
-
-                                break;
-
-                            case XmlPullParser.START_TAG:
-                                TagName = xpp.getName();
-
-                                if (TagName.equals("boxOfficeResult")) {  //상영기간과 ,조회하는해당 연도와 주차를 알수있다.
-
-                                } else if (TagName.equals("showRange")) {
-                                    xpp.next();
-                                    showRang = xpp.getText();
-                                } else if (TagName.equals("yearWeekTime")) {
-                                    xpp.next();
-                                    yearWeekTime = xpp.getText();
-                                }
+                    final String targetDt = se_et.getText().toString();
+                    //서버 주소 작성
+                    String address = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.xml"
+                            + "?key=" + apikey  //키값
+                            + "&targetDt=" + targetDt//조회하는 날짜
+                            + "&weekGb=0"    //주중으로 받아온다
+                            + "&itemPerPage=10"  //총10개의 데이터를 받아온다.
+                            + "&showRange"     //상영 기간을출력
+                            + "&yearWeekTime"  //조회하는 년도 주차를
+                            + "&rank"           //순위를 츨력을
+                            + "&movieCd"        //영화 대표코드
+                            + "&movieNm"        //영화 이름/
+                            + "&rankOldAndNew"  //영화가 새로들어왔는 기존의 있는지 확인
+                            + "&openDt"         //영화 개봉일 출력
+                            + "&audiCnt"        //해당 일 관객수
+                            + "&audiAcc";         //해당 일 누적관객수 출력
 
 
-                                if (TagName.equals("weeklyBoxOffice")) {
+                    try {
+                        URL url = new URL(address);
 
-                                    menber1 = new Menber();
-                                    menber1.setShowRang(showRang);
-                                    menber1.setYearWeekTime(yearWeekTime);
-
-                                } else if (TagName.equals("movieNm")) {
-                                    xpp.next();
-                                    name = xpp.getText();
-                                    menber1.setName(name);
-                                } else if (TagName.equals("rank")) {
-                                    xpp.next();
-                                    rank = xpp.getText();
-                                    menber1.setRank(rank);
-                                } else if (TagName.equals("movieCd")) {
-                                    xpp.next();
-                                    movieCd = xpp.getText();
-                                    menber1.setMovieCd(movieCd);
-                                } else if (TagName.equals("rankOldAndNew")) {
-                                    xpp.next();
-                                    rankOldAndNew = xpp.getText();
-                                    menber1.setRankOldAndNew(rankOldAndNew);
-                                } else if (TagName.equals("openDt")) {
-                                    xpp.next();
-                                    openDt = xpp.getText();
-                                    menber1.setOpenDt(openDt);
-                                } else if (TagName.equals("audiCnt")) {
-                                    xpp.next();
-                                    audiCnt = xpp.getText();
-                                    menber1.setAudiCnt(audiCnt);
-                                } else if (TagName.equals("audiAcc")) {
-                                    xpp.next();
-                                    audiAcc = xpp.getText();
-                                    menber1.setAudiAcc(audiAcc);
-                                }
+                        InputStream is = url.openStream();
+                        InputStreamReader isr = new InputStreamReader(is);
 
 
-                                break;
-
-                            case XmlPullParser.TEXT:
-                                break;
-
-                            case XmlPullParser.END_TAG:
-                                TagName = xpp.getName();
-
-                                if (TagName.equals("weeklyBoxOffice")) {
-
-                                    menbers.add(menber1);
+                        //xml분석
+                        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                        XmlPullParser xpp = factory.newPullParser();
+                        xpp.setInput(isr);
 
 
-                                }
+                        int eventType = xpp.getEventType();
+
+                        String TagName = null;
 
 
-                                break;
+                        while (eventType != XmlPullParser.END_DOCUMENT) {
+
+                            switch (eventType) {
+                                case XmlPullParser.START_DOCUMENT:
+
+                                    break;
+
+                                case XmlPullParser.START_TAG:
+                                    TagName = xpp.getName();
+
+                                    if (TagName.equals("boxOfficeResult")) {  //상영기간과 ,조회하는해당 연도와 주차를 알수있다.
+
+                                    } else if (TagName.equals("showRange")) {
+                                        xpp.next();
+                                        showRang = xpp.getText();
+                                    } else if (TagName.equals("yearWeekTime")) {
+                                        xpp.next();
+                                        yearWeekTime = xpp.getText();
+                                    }
 
 
-                        }
+                                    if (TagName.equals("weeklyBoxOffice")) {
+
+                                        menber1 = new Menber();
+                                        menber1.setShowRang(showRang);
+                                        menber1.setYearWeekTime(yearWeekTime);
+
+                                    } else if (TagName.equals("movieNm")) {
+                                        xpp.next();
+                                        name = xpp.getText();
+                                        menber1.setName(name);
+                                    } else if (TagName.equals("rank")) {
+                                        xpp.next();
+                                        rank = xpp.getText();
+                                        menber1.setRank(rank);
+                                    } else if (TagName.equals("movieCd")) {
+                                        xpp.next();
+                                        movieCd = xpp.getText();
+                                        menber1.setMovieCd(movieCd);
+                                    } else if (TagName.equals("rankOldAndNew")) {
+                                        xpp.next();
+                                        rankOldAndNew = xpp.getText();
+                                        menber1.setRankOldAndNew(rankOldAndNew);
+                                    } else if (TagName.equals("openDt")) {
+                                        xpp.next();
+                                        openDt = xpp.getText();
+                                        menber1.setOpenDt(openDt);
+                                    } else if (TagName.equals("audiCnt")) {
+                                        xpp.next();
+                                        audiCnt = xpp.getText();
+                                        menber1.setAudiCnt(audiCnt);
+                                    } else if (TagName.equals("audiAcc")) {
+                                        xpp.next();
+                                        audiAcc = xpp.getText();
+                                        menber1.setAudiAcc(audiAcc);
+                                    }
 
 
-                        xpp.next();
-                        eventType = xpp.getEventType();
+                                    break;
 
-                    }//while문 종료
+                                case XmlPullParser.TEXT:
+                                    break;
 
+                                case XmlPullParser.END_TAG:
+                                    TagName = xpp.getName();
 
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Toast.makeText(MainActivity.this, "검색 완료", Toast.LENGTH_SHORT).show();
+                                    if (TagName.equals("weeklyBoxOffice")) {
 
-                            adatper.notifyDataSetChanged();
-
-                        }
-                    });
+                                        menbers.add(menber1);
 
 
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (XmlPullParserException e) {
-                    e.printStackTrace();
+                                    }
+
+
+                                    break;
+
+
+                            }
+
+
+                            xpp.next();
+                            eventType = xpp.getEventType();
+
+                        }//while문 종료
+
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "검색 완료", Toast.LENGTH_SHORT).show();
+
+                                adatper.notifyDataSetChanged();
+
+                            }
+                        });
+
+
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (XmlPullParserException e) {
+                        e.printStackTrace();
+                    }
+
+
                 }
+            }.start();
 
 
-            }
-        }.start();
+            dialog.cancel();
 
-
-        dialog.cancel();
+        }
 
     }
 
